0220a802d9214f3ce12f6cbb58b823cb27d68720
pawanv048 <pawanv048@gmail.com>
2022-04-11 11:27:14 +0530

    inital commit

---
+++ App.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator
+
+} from 'react-native';
+
+
+
+//get data from this url
+const movieURL = "https://reactnative.dev/movies.json";
+
+const App = () => {
+//managing state with 'useState'
+  const [isLoading, setLoading] = React.useState(true)
+  const [data, setData] = React.useState([]);
+  const [title, setTitle] = React.useState([]);
+  const [description, setDescription] = React.useState([]);
+
+  //fetch data
+  useEffect(() => {
+    fetch(movieURL)
+      .then((response) => response.json()) //get response, convert to json
+      .then((json) => {
+        setData(json.movies);
+        setTitle(json.title);
+        setDescription(json.description);
+      })
+      .catch((error) => alert(error)) //display errors
+      .finally(setLoading(false)); // change loading state
+  }, []);
+  return (
+
+    <SafeAreaView style={styles.container}>
+      {/* while fetching data show indicator, else show response */}
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View style={{ borderBottomWidth : 1, marginBottom: 12}}>
+          {/* title from url */}
+          <Text>{title}</Text>
+          {/* description from url */}
+          <Text>{description}</Text>
+          {/* Display each movies */}
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+
+            <Text>
+              {item.title},
+              {item.releaseYear}
+            </Text>
+
+          )}
+        />
+        </View>
+      )}
+
+    </SafeAreaView>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+})
+
+export default App;
